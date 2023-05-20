646feb9258c861face90e94c61fd7d6d5c1e4b84
Vitalii-Dashivets <vdashivets1985@gmail.com>
2023-05-20 19:03:42 +0300

    com task2

---
+++ src/js/01-gallery.js
@@ -0,0 +1,37 @@
+// Add imports above this line
+// import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from  "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+
+console.log(galleryItems);
+
+const itemsPreviewMarkup = galleryItems.map(item =>
+ `<li class="gallery__item">
+  <a class="gallery__link" href=${item.original}>
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      alt='${item.description}'
+    />
+  </a>
+</li>`).join('');
+
+
+const optionsSet = {
+    captionDelay: 250,
+    captionsData: 'alt',
+    animationSpeed: 300,
+    swipeTolerance: 50,
+    fadeSpeed: 300,
+    scrollZoomFactor: 0.1,
+    }  
+
+const galleryBox = document.querySelector('.gallery');
+galleryBox.insertAdjacentHTML('beforeend', itemsPreviewMarkup);
+
+
+ new SimpleLightbox('.gallery a',  optionsSet);
