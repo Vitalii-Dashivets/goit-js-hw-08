21f4f95c2036fad2c792037eade01970580b2328
Vitalii-Dashivets <vdashivets1985@gmail.com>
2023-05-21 10:33:46 +0300

    Edit task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,36 @@
-console.log('222222');
- import Player from '@vimeo/player';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+    
+player.on('timeupdate', throttle( function({seconds}){
+    
+    const currentTime = seconds;
+    console.log(currentTime);
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime));
+       
+},2000));
+    
+   
+const currentTime = localStorage.getItem("videoplayer-current-time");
+const parsedCurrentTime = JSON.parse(currentTime);
+// console.log(currentTime);
+// console.log(parsedCurrentTime);
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+player.setCurrentTime(parsedCurrentTime).then(function (seconds) {
+    
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
